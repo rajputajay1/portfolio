3e578a3b149f7bfa335ce1da30f0a660a1732fb1
ajayrajuput <120632665+ajayrajuput@users.noreply.github.com>
2025-02-16 16:33:28 +0530

    all

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Enables strict mode for React
-  swcMinify: true, // Enables SWC minification for faster builds
-};
+const nextConfig = {};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true, // Enables strict mode for React
+//   swcMinify: true, // Enables SWC minification for faster builds
+// };
+
+// export default nextConfig;
